2e02fc469c521981f13e41c4c98aa42398f4dc63
Odunayo Adekoya <odunayo.adekoya@yahoo.com>
2024-04-24 17:37:44 -0400

    Added React

---
+++ fe-test/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        axios.get('/api/data')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Backend Data</h1>
+            <p>{JSON.stringify(data)}</p>
+        </div>
+    );
+}
+
+export default App;
